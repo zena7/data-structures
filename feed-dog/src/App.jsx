ac9c9dccf6a97d79fa0b19e70861cfeb0aef8da4
Nataly Zernova <nz_18@mail.ru>
2024-06-04 19:14:36 +0300

    feature: work with coordinates and add food block div

--- feed-dog/src/App.jsx
+++ feed-dog/src/App.jsx
@@ -1,74 +1,77 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import "./App.css";
-import mainDog from "./assets/mainDog.png";
-
+import * as classes from "./App.css";
+// import cryingDog from "./assets/cryingDog.png";
+// import happyDog from "./assets/happyDog.png";
+// import mainDog from "./assets/mainDog.png";
+import { DogPlayer } from "./dogPlayer/dogPlayer";
 function App() {
   const windowWidth = window.innerWidth;
 
-  const [count, setCount] = useState(0);
   // const [food, setfood] = useState([]);
-  const [shift, setShift] = useState(0);
-  // const [leftСoordinate, setLeftСoordinate] = useState(0);
-  // const [rightСoordinate, setRightСoordinate] = useState(0);
-  const playerRef = useRef(null);
-  const marginOfError = 10;
+  // const [shift, setShift] = useState(0);
+  const [leftСoordinate, setLeftСoordinate] = useState(0);
+  const [rightСoordinate, setRightСoordinate] = useState(0);
 
-  // const windowHeight = window.innerHeight;
+  const [score, setScore] = useState(0);
+  const foodRef = useRef(null);
+  // const marginOfError = 10;
+
+  function setCoordinates(value, direction) {
+    if (direction === "left") {
+      setLeftСoordinate(value);
+    } else {
+      setRightСoordinate(value);
+      console.log("This value from APP", rightСoordinate);
+    }
+  }
+
+  // useEffect(
+  //   () => console.log("LEFT:", leftСoordinate, "RIGHT:", rightСoordinate),
+  //   [leftСoordinate, rightСoordinate]
+  // );
+
+  const windowHeight = window.innerHeight;
+  console.log("Height window is", windowHeight);
   // const windowWidth = window.innerWidth;
-
-  function onHandleClick() {
-    setCount((prev) => prev + 1);
-  }
-
-  function onHandleClickMinus() {
-    setCount((prev) => (prev > 0 ? prev - 1 : 0));
-  }
 
   // function getRandom(min = 0, max = 1) {
   //   return Math.round((Math.random() * (max - min) + min) * 10);
   // }
 
-  useEffect(() => {
-    const step = 4;
+  // function onHandleStartGame() {
+  //   console.log("Let's start");
+  // }
+  // useEffect(() => {
+  //   let rightCoordOfFood = foodRef.current.getBoundingClientRect().right;
+  //   // console.log("food right:", rightCoordOfFood);
 
-    document.addEventListener("keydown", function (event) {
-      let rightCoord = playerRef.current.getBoundingClientRect().right;
-      let leftCoord = playerRef.current.getBoundingClientRect().left;
+  //   let idInterval = setInterval(() => {
+  //     let topCoordOfFood = foodRef.current.getBoundingClientRect().top;
+  //     console.log("IN INTERVAL", topCoordOfFood, rightСoordinate);
+  //     if (topCoordOfFood >= 570) {
+  //       console.log("food:", rightCoordOfFood, "dog:", rightСoordinate);
+  //     }
+  //   }, 1500);
 
-      if (
-        event.code === "ArrowRight" &&
-        rightCoord < windowWidth - marginOfError
-      ) {
-        setShift((prev) => {
-          let newShift = prev - step;
-          playerRef.current.style.right = `${newShift}%`;
-          return newShift;
-        });
-      } else if (event.code === "ArrowLeft" && leftCoord > 0 + marginOfError) {
-        setShift((prev) => {
-          let newShift = prev + step;
-          playerRef.current.style.right = `${newShift}%`;
-          return newShift;
-        });
-      }
-    });
-  }, []);
-
-  function onHandleStartGame() {
-    console.log("Let's start");
-  }
+  //   setTimeout(() => {
+  //     clearInterval(idInterval);
+  //     console.log("CLEAR");
+  //   }, 3000);
+  // });
 
   return (
     <>
-      <p className='count'>{count}</p>
-      <button onClick={onHandleClick}>Add</button>
-      <button onClick={onHandleClickMinus}>Minus</button>
-      <button onClick={onHandleStartGame}>Start game</button>
-      <div className='player' ref={playerRef}>
-        <img src={mainDog} alt='iconOfMainDog'></img>
+      <div className='scoreContainer'>
+        <p>Score {score}</p>
       </div>
+      <div
+        className={`${classes.food} ${classes.healthyFood}`}
+        ref={foodRef}></div>
+      <div className='food dangerFood'></div>
+      {/* <button onClick={onHandleStartGame}>Start game</button> */}
+      <DogPlayer windowWidth={windowWidth} setCoordinates={setCoordinates} />
     </>
   );
 }
